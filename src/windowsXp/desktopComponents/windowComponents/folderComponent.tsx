58517c3aae2ba432dd4a6041cd005e9693949c4c
maryanne <kaffermaryanne@gmail.com>
2025-07-13 23:36:18 -0300

    responsive folders

--- src/windowsXp/desktopComponents/windowComponents/folderComponent.tsx
+++ src/windowsXp/desktopComponents/windowComponents/folderComponent.tsx
@@ -14,45 +14,46 @@ export default function FolderComponent({ name }: { name: string }) {
             <div className="bg-yellow-100 bg-opacity-50 h-fit w-full flex flex-col text-black text-lg">
                 <span className="border-b border-[1px] border-gray-300">
                     {options.map((option, index) => (
-                        <button onMouseEnter={() => setisHoveringOpt(index)} onMouseLeave={() => setisHoveringOpt(-1)} key={option} className={`${isHoveringOpt === index && `${currentAppearance.color} text-white`} px-3`}>
+                        <button onMouseEnter={() => setisHoveringOpt(index)} onMouseLeave={() => setisHoveringOpt(-1)} key={option}
+                            className={`${isHoveringOpt === index && `${currentAppearance.color} text-white`} md:px-3 px-2`}>
                             {option}
                         </button>
                     ))}
                 </span>
-                <div className="px-2 py-1 flex border-b border-[1px] border-gray-300 h-[40px]">
+                <div className="md:px-2 py-1 flex border-b border-[1px] border-gray-300 md:h-[40px] h-[30px]">
                     {folderOptions.map((option, index) => (
                         <>
                             <button onMouseEnter={() => setisHoveringCnf(index)} onMouseLeave={() => setisHoveringCnf(-1)}
-                                className={`flex items-center gap-3 p-1 relative active:brightness-75 cursor-default ${isHoveringCnf === index && `${currentAppearance.color} text-white`}`}>
-                                <img draggable={false} src={option.img} className="size-6" />{option.name} {option.list && <span className="top-[50%] -ml-2 relative text-xl">🢓</span>}
+                                className={`flex items-center md:gap-3 gap-1 p-1 relative active:brightness-75 cursor-default ${isHoveringCnf === index && `${currentAppearance.color} text-white`}`}>
+                                <img draggable={false} src={option.img} className="md:size-6 size-4" /><p className="md:text-lg text-sm leading-[0.7]">{option.name}</p> {option.list && <span className="md:top-[50%] top-[85%] md:-ml-2 -ml-1 relative text-xl">🢓</span>}
                             </button>
-                            {option.divider && <div className="h-full w-[2px] bg-gray-300 mx-1"></div>}
+                            {option.divider && <div className="h-full md:w-[2px] w-[1px] bg-gray-300 mx-1"></div>}
                         </>
                     ))}
                 </div>
-                <div className="flex border-b border-[1px] border-gray-300 px-2 gap-2 relative h-[30px] items-center">
+                <div className="flex md:text-lg text-sm border-b border-[1px] border-gray-300 px-2 gap-2 relative h-[30px] items-center">
                     <h1>Address</h1>
-                    <span className={`border-[1px] ${currentAppearance.border} px-1 w-full h-[85%] items-center flex`}>
-                        <img draggable={false} src="/icons/folderClosedIcon.png" className="size-5" />
-                        <input defaultValue={`C:\\Desktop\\${name}`} type="text" className="w-full h-[85%] bg-white" />
+                    <span className={`border-[1px] ${currentAppearance.border} px-1 w-full md:h-[85%] h-[75%] items-center flex`}>
+                        <img draggable={false} src="/icons/folderClosedIcon.png" className="md:size-5 size-4" />
+                        <input defaultValue={`C:\\Desktop\\${name}`} type="text" className="w-full md:h-[85%] h-[75%] bg-white" />
                     </span>
-                    <button className="flex items-center gap-2 relative active:brightness-75 cursor-default mr-2">
-                        <img draggable={false} src="/icons/GoIcon.png" className="size-6" /> Go
+                    <button className="flex items-center md:gap-2 gap-1 relative active:brightness-75 cursor-default mr-2">
+                        <img draggable={false} src="/icons/GoIcon.png" className="md:size-6 size-4" /> Go
                     </button>
                 </div>
             </div>
             <div className="flex w-full h-full">
-                <div className={`h-full w-[15rem] ${currentAppearance.color} flex flex-col bg-opacity-80 p-4 gap-4`}>
+                <div className={`h-full md:w-[15rem] w-[9rem] ${currentAppearance.color} flex flex-col bg-opacity-80 md:p-4 p-3 md:gap-4 gap-3`}>
                     {folderConfig.map((option) => (
                         <div key={option.name} className={`flex flex-col ${currentAppearance.mainColor}`}>
-                            <span className="bg-gradient-to-r flex items-center justify-between from-white via-white to-transparent text-xl px-2 rounded-t-sm border-[2px] border-gray-300">
+                            <span className="bg-gradient-to-r flex items-center justify-between from-white via-white to-transparent md:text-xl text-md leading-[0.7] md:px-2 px-1 rounded-t-sm md:border-[2px] border-[1px] border-gray-300">
                                 {option.name}
-                                <FaAngleDoubleUp size={18} className={`bg-white active:brightness-75 rounded-full border-[1px] border-gray-300 shadow-black shadow-lg ${currentAppearance.color}`} />
+                                <FaAngleDoubleUp className={`md:size-[18px] size-4 bg-white active:brightness-75 rounded-full border-[1px] border-gray-300 shadow-black shadow-lg ${currentAppearance.color}`} />
                             </span>
-                            {option.content && <span className="bg-white bg-opacity-70 flex flex-col p-2 border-[2px] border-gray-300">
+                            {option.content && <span className="bg-white bg-opacity-70 flex flex-col md:p-2 px-2 py-1 border-[2px] border-gray-300">
                                 {option.content.map((item) => (
-                                    <button key={item.name} className="flex items-center gap-2 relative active:brightness-75 cursor-default ml-3 text-lg text-left leading-[0.8]">
-                                        <img draggable={false} src={item.img} className="size-5" />{item.name}
+                                    <button key={item.name} className="flex items-center gap-2 relative active:brightness-75 cursor-default md:ml-3 md:text-lg text-sm text-left leading-[0.8]">
+                                        <img draggable={false} src={item.img} className="md:size-5 size-3" />{item.name}
                                     </button>
                                 ))}
                             </span>}
